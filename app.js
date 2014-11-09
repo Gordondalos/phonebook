b88657412ffbb2025bd4f9f0d99a6ffefcd091f8
Radik Fattakhov <radikft@gmail.com>
2014-11-09 22:36:58 +0300

    step-06

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 var app = angular.module('myApp', []);
 
-app.factory('MyLogginService', function(){
-    return {
-        log: function (argument) {
-            console.log(argument);
-        }
+app.controller('MainController', ['$scope',
+    function($scope){
+        
+}]);
+
+app.filter('decorator', function () {
+    return function(input, symbol){
+        return symbol + " " + input + " " + symbol;
     };
 });
-
-app.factory('AlertLogginService', ['$window',function ($window) {
-    return {
-        log: function (argument) {
-            $window.alert(argument);
-        }
-    };
-}]);
-
-app.controller('MainController', ['$scope', 'AlertLogginService',
-    function($scope, logger){
-    $scope.name = 'Guest';
-
-    $scope.myFunc = function(){
-        logger.log('Hello!');
-    };
-}]);
-
-app.factory('SharedData', function(){
-    return {
-        someData: 'Test'
-    };
-});
-
-app.controller('Test1', ['$scope', 'SharedData', function($scope, sharedData){
-    $scope.data = sharedData;
-}]);
-
-app.controller('Test2', ['$scope', 'SharedData', function($scope, sharedData){
-    $scope.changeData = function(){
-        sharedData.someData = 'Data Changed!';
-    };
-}]);
-
 
 
 
