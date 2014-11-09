00f55d6060a1fb34662e4f14ed56e80a043c1af8
Radik Fattakhov <radikft@gmail.com>
2014-11-09 21:57:29 +0300

    step-04

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-angular.module('myApp', []);
+var app = angular.module('myApp', []);
+
+app.factory('MyLogginService', function(){
+    return {
+        log: function (argument) {
+            console.log(argument);
+        }
+    };
+});
+
+app.factory('AlertLogginService', function () {
+    return {
+        log: function (argument) {
+            window.alert(argument);
+        }
+    };
+});
+
+app.controller('MainController', ['$scope', 'AlertLogginService',
+    function($scope, logger){
+    $scope.name = 'Guest';
+
+    $scope.myFunc = function(){
+        logger.log('Hello!');
+    };
+}]);
